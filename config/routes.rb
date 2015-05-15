6f2e9bf66fcdb05791cfb3b1e66176c9cfdcc4e0
Zack Anderson <zack@hush.es>
2015-05-14 19:36:25 -0500

    basic scaffolding
    add basic blog routes and controller. also write basic sidebar markup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'blog/index'
+
+  root 'blog#index'
+  get 'blog/' => 'blog#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
